b7fd62bff24937c989b426a67ae53939b0946599
meriy100 <ttattataa@gmail.com>
2016-04-14 13:43:27 +0900

    Better and karabina

--- generate.sh
+++ generate.sh
@@ -3,3 +3,5 @@ ln -sf ~/home/.zshrc ~/.zshrc
 ln -sf ~/home/.zshrc.alias ~/.zshrc.alias
 ln -sf ~/home/.vimrc ~/.vimrc
 ln -sf ~/home/.tmux.conf ~/.tmux.conf
+ln -sf ~/home/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+ln -sf ~/home/BetterTouchTool/ ~/Library/Application\ Support/BetterTouchTool
