7dfa3f84659cd4e827e8ae66b6f74b87ce0e8094
meriy100 <ttattataa@gmail.com>
2016-05-12 11:38:06 +0900

    home to dotfiles

--- generate.sh
+++ generate.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-ln -sf ~/home/.zshrc ~/.zshrc
-ln -sf ~/home/.zshrc.alias ~/.zshrc.alias
-ln -sf ~/home/.vimrc ~/.vimrc
-ln -sf ~/home/.tmux.conf ~/.tmux.conf
-ln -sf ~/home/private.xml ~/Library/Application\ Support/Karabiner/private.xml
-ln -sf ~/home/BetterTouchTool/ ~/Library/Application\ Support/
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/private.xml ~/Library/Application\ Support/Karabiner/private.xml
 
-ln -sf ~/home/.vimrc.set ~/.vim/.vimrc.set
-ln -sf ~/home/.vimrc.keymap ~/.vim/.vimrc.keymap
-ln -sf ~/home/.vimrc.plugins ~/.vim/.vimrc.plugins
-ln -sf ~/home/.vimrc.script ~/.vim/.vimrc.script
+ln -sf ~/dotfiles/.vimrc.set ~/.vim/.vimrc.set
+ln -sf ~/dotfiles/.vimrc.keymap ~/.vim/.vimrc.keymap
+ln -sf ~/dotfiles/.vimrc.plugins ~/.vim/.vimrc.plugins
+ln -sf ~/dotfiles/.vimrc.script ~/.vim/.vimrc.script
